c0962d21aec6309f496a634e667fdb0ed0a3c449
nanagitari <nanagitari18@gmail.com>
2024-11-03 23:23:06 +0300

    added new files

---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+    <p>© 2023 City Lovers</p>
+  </footer>
+  );
+}
+
+export default Footer;
+
+
+
+
+
+
+
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Content() {
+  return (
+    <main>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
+
+
+
+
+
+
